6739d3a65c6c03869ebd08cffc266d6cd0c23955
Erik Kassubek <53377037+ErikKassubek@users.noreply.github.com>
2022-05-16 21:08:46 +0200

    move lock and unlock functions

--- mutex.go
+++ mutex.go
@@ -6,3 +6,24 @@ import "sync"
 type Mutex struct {
 	mu sync.Mutex
 }
+
+// Lock mutex m in routine r
+// TODO: change so that r is calculated and taken from Routines
+func (m *Mutex) Lock(r *Routine) {
+	defer m.mu.Lock()
+
+	// if detection is disabled
+	if !Opts.RunDetection {
+		return
+	}
+
+	// update data structures
+	r.updateLock(m)
+
+}
+
+// Unlock mutex m
+func (m *Mutex) Unlock() {
+	defer m.mu.Unlock()
+
+}
--- undead.go
+++ undead.go
@@ -5,27 +5,6 @@ func Initialize() {
 
 }
 
-// Lock mutex m in routine r
-// TODO: change so that r is calculated and taken from Routines
-func Lock(m Mutex, r *Routine) {
-	defer m.mu.Lock()
-
-	// if detection is disabled
-	if !Opts.RunDetection {
-		return
-	}
-
-	// update data structures
-	r.updateLock(&m)
-
-}
-
-// Unlock mutex m
-func Unlock(m Mutex) {
-	defer m.mu.Unlock()
-
-}
-
 // run periodical deadlock detection check
 func PeriodicalDetection() {
 
