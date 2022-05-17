d87e17157e49ba7a795d3eef73ef2a957c0debf8
Erik Kassubek <53377037+ErikKassubek@users.noreply.github.com>
2022-05-17 14:18:17 +0200

    only update trees if more than one routine is running

--- mutex.go
+++ mutex.go
@@ -1,6 +1,9 @@
 package undead
 
-import "sync"
+import (
+	"runtime"
+	"sync"
+)
 
 // type to implement a lock
 type Mutex struct {
@@ -17,8 +20,10 @@ func (m *Mutex) Lock(r *Routine) {
 		return
 	}
 
-	// update data structures
-	r.updateLock(m)
+	// update data structures if more than on routine is running
+	if runtime.NumGoroutine() > 1 {
+		r.updateLock(m)
+	}
 
 }
 
