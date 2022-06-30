e0538b8d21f445234753170ee5ff3bfa8f74b2e8
Erik Kassubek <53377037+ErikKassubek@users.noreply.github.com>
2022-06-30 18:16:22 +0200

    add additional comments for mutexInt interface defination

--- mutexInt.go
+++ mutexInt.go
@@ -38,12 +38,21 @@ It also implements code which is used for both mutex and rw-mutex
 
 // creat and interface for Mutex and RWMutex
 type mutexInt interface {
+	// getter for isLocked
 	getNumberLocked() *int
+	// getter for isLockedRoutineIndex
 	getIsLockedRoutineIndex() *int
+	// getter for context
 	getContext() *[]callerInfo
+	// getter for memoryPosition
 	getMemoryPosition() uintptr
+	// getter for in(itialized)
 	getIn() *bool
+	// getter for mu
+	// 	if bool is true, *sync.Mutex was returned, *sync.RWMutex is nil
+	// 	if bool is false, *sync.Mutex is nil, *sync.RWMutex ware returned
 	getLock() (bool, *sync.Mutex, *sync.RWMutex)
+	//getter for isRead
 	getIsRead() *bool
 }
 
@@ -57,7 +66,7 @@ func lockInt(m mutexInt, rLock bool) {
 	// panic if the lock was not initialized
 	if !*m.getIn() {
 		errorMessage := fmt.Sprint("Lock ", &m, " was not created. Use ",
-			"x := NewLock()")
+			"x := NewLock().")
 		panic(errorMessage)
 	}
 
@@ -110,6 +119,73 @@ func lockInt(m mutexInt, rLock bool) {
 	if numRoutine > 1 {
 		(*r).updateLock(m)
 	}
+}
+
+// try to lock the mutex or rw-mutex and update the detector data.
+// The lock is only acquired, if it is available at the time of the call
+//  Args:
+//   m (mutexInt): mutex or rw-mutex to lock
+//   rLock (bool): if set to true, the lock is a reader lock
+//  Returns:
+//   (bool): true if the acquisition was successful, false otherwise
+func tryLockInt(m mutexInt, rLock bool) bool {
+	// panic if the lock was not initialized
+	if !*m.getIn() {
+		errorMessage := fmt.Sprint("Lock ", &m, " was not created. Use ",
+			"x := NewLock()")
+		panic(errorMessage)
+	}
+
+	// initialize detector if necessary
+	if !initialized {
+		initialize()
+	}
+
+	// try to lock mu
+	d, l, t := m.getLock()
+	var res bool
+	if d {
+		// lock if m is mutex
+		res = l.TryLock()
+	} else {
+		// lock if m is rw-mutex
+		if rLock {
+			res = t.TryRLock()
+		} else {
+			res = t.TryLock()
+		}
+	}
+
+	// if locking was successful increase numberLocked
+	if res {
+		*m.getNumberLocked() += 1
+	}
+
+	// return if detection is disabled
+	if !opts.periodicDetection && !opts.comprehensiveDetection {
+		return res
+	}
+
+	// initialize routine if necessary
+	index := getRoutineIndex()
+	if index == -1 {
+		// create new routine, if not initialized
+		newRoutine()
+	}
+	index = getRoutineIndex()
+
+	r := &routines[index]
+
+	*m.getIsLockedRoutineIndex() = index
+
+	// update data structures if more than on routine is running
+	if runtime.NumGoroutine() > 1 {
+		if res {
+			(*r).updateTryLock(m)
+		}
+	}
+
+	return res
 }
 
 // unlock the mutex or rw-mutex and update the detector data
